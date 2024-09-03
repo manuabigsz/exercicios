862c1510183e8d4bec91deccfdf5ac76c254a741
manuabigsz <manuelaossanes@hotmail.com>
2024-09-03 16:23:17 -0300

    main

--- 5inverteString.py
+++ 5inverteString.py
@@ -1,20 +1,15 @@
 def inverter_string(string):
-
     caracteres = list(string)
-    
     inicio = 0
     fim = len(caracteres) - 1
-    
  
     while inicio < fim:
     
         caracteres[inicio], caracteres[fim] = caracteres[fim], caracteres[inicio]
         
- 
         inicio += 1
         fim -= 1
     
-
     return ''.join(caracteres)
 
 string_original = input("Digite uma string para inverter: ")
@@ -22,5 +17,4 @@ string_original = input("Digite uma string para inverter: ")
 
 resultado = inverter_string(string_original)
 
-# Exibe o resultado
 print(f"String invertida: {resultado}")
